b18a6398bcbd37686fbee27eb03d0686d9ccd894
LukeSchmude <schmude2002@gmail.com>
2021-08-20 10:20:48 -0500

    first push

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ myFunction();
     
 For example, `summation(4)` should return 10 because 1+2+3+4 is 10. Note, you may use a for loop for this function if you wish */
 
-function summation(/*Your Code Here*/) {
-  /*Your Code Here*/
-
+function summation(num1) {
+  for(let i = 0; i < 5; i++)
+  return num1 + i
+console.log(summation('4'))
   }
  
 
